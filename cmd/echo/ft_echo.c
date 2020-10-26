bd6672355f52d89942c1e1e89a470ffbeb806f8d
Kiss Gautier <gkiss00>
2020-10-26 20:50:36 +0100

    echo no more segfault

--- cmd/echo/ft_echo.c
+++ cmd/echo/ft_echo.c
@@ -5,7 +5,7 @@ void        ft_echo(t_data *data)
     int     i;
 
     i = 0;
-    while(data->cmd_tab && data->cmd_tab[0] && data->cmd_tab[0]->arg[i])
+    while(data->cmd_tab && data->cmd_tab[0] && data->cmd_tab[0]->arg && data->cmd_tab[0]->arg[i])
     {
         ft_putstr_fd(data->cmd_tab[0]->arg[i], 1);
         if (data->cmd_tab[0]->arg[i + 1])
