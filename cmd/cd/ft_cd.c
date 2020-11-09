ef4f1436cf34b7dc55091f4874cdd49eb9d47c3e
Kiss Gautier <gkiss00>
2020-11-09 20:53:32 +0100

    cd redone

--- cmd/cd/ft_cd.c
+++ cmd/cd/ft_cd.c
@@ -3,12 +3,8 @@
 static int ft_go(t_data *data)
 {
     char *tmp;
-    char *t;
 
-    if ((t = ft_transform(data->cmd_tab[data->a]->arg[0])) == NULL)
-        return (ft_error(NULL, MALLOC));
-    tmp = ft_get_path(data->path, t);
-    free(t);
+    tmp = ft_get_path(data->path, data->cmd_tab[data->a]->arg[0]);
     if(tmp == NULL)
         return (ft_error(NULL, MALLOC));
     if (ft_does_dir_exist(tmp) == 0)
