425a3973aba14c5e4385bc596d5551d90b4f3764
Kiss Gautier <gkiss00>
2020-11-10 11:37:23 +0100

    hub better

--- hub/ft_hub.c
+++ hub/ft_hub.c
@@ -7,16 +7,16 @@ void     ft_hub(t_data *data, char *cmd)
     cmd_maj = ft_tomaj(cmd);
     if (ft_strcmp(cmd, "exit") == 0)
         ft_export(data);
-    if (ft_strcmp(cmd, "export") == 0)
+    else if (ft_strcmp(cmd, "export") == 0)
         ft_export(data);
-    if (ft_strcmp(cmd, "unset") == 0)
+    else if (ft_strcmp(cmd, "unset") == 0)
         ft_export(data);
-    if (ft_strcmp(cmd, "cd") == 0)
+    else if (ft_strcmp(cmd, "cd") == 0)
         ft_export(data);
-    if (ft_strcmp(cmd_maj, "ECHO") == 0)
+    else if (ft_strcmp(cmd_maj, "ECHO") == 0)
         ft_echo(data);
-    if (ft_strcmp(cmd_maj, "PWD") == 0)
+    else if (ft_strcmp(cmd_maj, "PWD") == 0)
         ft_echo(data);
-    if (ft_strcmp(cmd_maj, "ENV") == 0)
+    else if (ft_strcmp(cmd_maj, "ENV") == 0)
         ft_echo(data);
 }
